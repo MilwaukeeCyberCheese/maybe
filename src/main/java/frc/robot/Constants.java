295cbcf2c607b198792c0a0af8ddc8dca62f59a3
mkecybercheese <mkecybercheese@gmail.com>
2023-02-06 17:02:39 -0600

    change constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,69 +12,75 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public final class Constants {
 
-public static final class sensors{
-        public static final int LEFT_ENCODER_A = 0;
-        public static final int LEFT_ENCODER_B = 1;
-        public static final int RIGHT_ENCODER_A = 2;
-        public static final int RIGHT_ENCODER_B = 3;
-        public static final int GYRO = 0;
+        public static final class sensors {
+                public static final int LEFT_ENCODER_A = 0;
+                public static final int LEFT_ENCODER_B = 1;
+                public static final int RIGHT_ENCODER_A = 2;
+                public static final int RIGHT_ENCODER_B = 3;
+                public static final int GYRO = 0;
+        }
+
+        public static final class pneumatics {
+                // public static final Solenoid lSolenoid = new
+                // Solenoid(PneumaticsModuleType.CTREPCM, 0);
+                // public static final Solenoid rSolenoid = new
+                // Solenoid(PneumaticsModuleType.CTREPCM, 1);
+                public static final DoubleSolenoid lSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+                public static final DoubleSolenoid rSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+        }
+
+        public static final class controllers {
+                public static final int DRIVETRAIN_LEFT_FRONT_SPARK = 1; // front = closest to intake, left = facing
+                                                                         // intake
+                public static final int DRIVETRAIN_LEFT_REAR_SPARK = 2;
+                public static final int DRIVETRAIN_RIGHT_FRONT_SPARK = 3;
+                public static final int DRIVETRAIN_RIGHT_REAR_SPARK = 4;
+                public static final int LEFT_INTAKE_SPARK = 5;
+                public static final int RIGHT_INTAKE_SPARK = 6;
+                public static final int ELEVATOR_SPARK = 7;
+
+                /**
+                 * These static objects are used throughout the program.
+                 * Access this object using the following code, for example:
+                 * Constants.controllers.leftFrontSpark
+                 */
+                public static final CANSparkMax leftFrontSpark = new CANSparkMax(DRIVETRAIN_LEFT_FRONT_SPARK,
+                                MotorType.kBrushed);
+                public static final CANSparkMax leftRearSpark = new CANSparkMax(DRIVETRAIN_LEFT_REAR_SPARK,
+                                MotorType.kBrushed);
+                public static final CANSparkMax rightFrontSpark = new CANSparkMax(DRIVETRAIN_RIGHT_FRONT_SPARK,
+                                MotorType.kBrushed);
+                public static final CANSparkMax rightRearSpark = new CANSparkMax(DRIVETRAIN_RIGHT_REAR_SPARK,
+                                MotorType.kBrushed);
+                public static final CANSparkMax leftIntakeSpark = new CANSparkMax(LEFT_INTAKE_SPARK,
+                                MotorType.kBrushless);
+                public static final CANSparkMax rightIntakeSpark = new CANSparkMax(RIGHT_INTAKE_SPARK,
+                                MotorType.kBrushless);
+                public static final CANSparkMax elevatorSpark = new CANSparkMax(ELEVATOR_SPARK,
+                                MotorType.kBrushless);
+        }
+
+        public static final class drive {
+                public static final MotorControllerGroup m_leftMotorController = new MotorControllerGroup(
+                                controllers.leftFrontSpark, controllers.leftRearSpark);
+                public static final MotorControllerGroup m_rightMotorController = new MotorControllerGroup(
+                                controllers.rightFrontSpark, controllers.rightRearSpark);
+
+                public static final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotorController,
+                                m_rightMotorController);
+
+                public static final double DRIVE_SPEED = 0.5;
+
+                public static final boolean LEFT_FRONT_INVERTED = true;
+                public static final boolean LEFT_REAR_INVERTED = false;
+                public static final boolean RIGHT_FRONT_INVERTED = false;
+                public static final boolean RIGHT_REAR_INVERTED = true;
+        }
+
+        public static final class intake {
+                public static final boolean LEFT_INVERTED = true;
+                public static final boolean RIGHT_INVERTED = false;
+
+                public static final double INTAKE_SPEED = 0.5;
+        }
 }
-
-    public static final class pneumatics{
-        // public static final Solenoid lSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
-        // public static final Solenoid rSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
-        public static final DoubleSolenoid lSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
-        public static final DoubleSolenoid rSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
-    }
-
-    public static final class controllers {
-        public static final int DRIVETRAIN_LEFT_FRONT_SPARK = 1; // front = closest to intake, left = facing intake
-        public static final int DRIVETRAIN_LEFT_REAR_SPARK = 2;
-        public static final int DRIVETRAIN_RIGHT_FRONT_SPARK = 3;
-        public static final int DRIVETRAIN_RIGHT_REAR_SPARK = 4;
-        public static final int LEFT_INTAKE_SPARK = 5;
-        public static final int RIGHT_INTAKE_SPARK = 6;
-        public static final int ELEVATOR_SPARK = 7;
-
-        /**
-         * These static objects are used throughout the program.
-         * Access this object using the following code, for example:
-         * Constants.controllers.leftFrontSpark
-         */
-        public static final CANSparkMax leftFrontSpark = new CANSparkMax(DRIVETRAIN_LEFT_FRONT_SPARK,
-                MotorType.kBrushed);
-        public static final CANSparkMax leftRearSpark = new CANSparkMax(DRIVETRAIN_LEFT_REAR_SPARK, 
-                MotorType.kBrushed);
-        public static final CANSparkMax rightFrontSpark = new CANSparkMax(DRIVETRAIN_RIGHT_FRONT_SPARK,
-                MotorType.kBrushed);
-        public static final CANSparkMax rightRearSpark = new CANSparkMax(DRIVETRAIN_RIGHT_REAR_SPARK,
-                MotorType.kBrushed);
-        public static final CANSparkMax leftIntakeSpark = new CANSparkMax(LEFT_INTAKE_SPARK, 
-                MotorType.kBrushless);
-        public static final CANSparkMax rightIntakeSpark = new CANSparkMax(RIGHT_INTAKE_SPARK, 
-                MotorType.kBrushless);
-        public static final CANSparkMax elevatorSpark = new CANSparkMax(ELEVATOR_SPARK,
-                MotorType.kBrushless);        
-        }
-
-        public static final class drive{
-            public static final MotorControllerGroup m_leftMotorController = new MotorControllerGroup(controllers.leftFrontSpark, controllers.leftRearSpark);
-            public static final MotorControllerGroup m_rightMotorController = new MotorControllerGroup(controllers.rightFrontSpark, controllers.rightRearSpark);
-
-            public static final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotorController, m_rightMotorController);
-
-            public static final double DRIVE_SPEED = 0.3;
-
-        public static final boolean LEFT_FRONT_INVERTED = true;
-        public static final boolean LEFT_REAR_INVERTED = false;
-        public static final boolean RIGHT_FRONT_INVERTED = false;
-        public static final boolean RIGHT_REAR_INVERTED = true;
-        }
-
-        public static final class intake{
-            public static final boolean LEFT_INVERTED = true;
-            public static final boolean RIGHT_INVERTED = false;
-
-            public static final double INTAKE_SPEED = 0.5;
-        }
-    }
