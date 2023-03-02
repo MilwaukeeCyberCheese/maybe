17f155049df155faa0218565a5503d88b8eca4c2
Sam <techguy763@gmail.com>
2023-03-02 17:49:47 +0000

    change to one subsystem for intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,12 +1,15 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Intake extends SubsystemBase{
- 
-private double speed;
+public class Intake extends SubsystemBase {
+
+    private double speed;
+    private Value position;
+
     /**
      * Creates a new ExampleSubsystem.
      */
@@ -20,17 +23,20 @@ private double speed;
         log();
     }
 
-
-
-
-    
     public void drive(double speed) {
         this.speed = speed;
         Constants.controllers.intakeSpark.set(speed);
     }
 
-    
+    public void setPosition(Value position) {
+        Constants.pneumatics.lIntakeSolenoid.set(position);
+        Constants.pneumatics.rIntakeSolenoid.set(position);
+
+    }
+
     public void log() {
         SmartDashboard.putNumber("IntakeSpeed", speed);
+        SmartDashboard.putData(Constants.pneumatics.lIntakeSolenoid);
+        SmartDashboard.putData(Constants.pneumatics.rIntakeSolenoid);
     }
 }
