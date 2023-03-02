5d4e6b02d75ef5558f29c9879256dd3fd14cb301
Sam <techguy763@gmail.com>
2023-03-02 17:51:56 +0000

    add remainder of intake commands

---
+++ src/main/java/frc/robot/commands/IntakeOff.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/* Shifts the drivetrain into first gear */
+public class IntakeOff extends CommandBase {
+  private final Intake m_intake;
+
+  public IntakeOff(Intake intake) {
+    m_intake = intake;
+    addRequirements(m_intake);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  public void initialize() {
+    m_intake.setPosition(Constants.intake.intakeOff);
+  }
+
+  
+}
